f4c107764e307e42611b47be792e99f65b8aafb0
Ashish Singh <singhashish.cs@gmail.com>
2025-05-20 17:14:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+
+
+var allbutton = document.querySelectorAll(".drum").length;
+for (var i = 0; i < allbutton; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        var buttonInnerHtml = this.innerHtml;
+
+        switch (buttonInnerHtml) {
+            case "w":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+
+            case "a":
+                var audio = new Audio("sounds/kick.mp3");
+                audio.play();
+                break;
+
+            case "s":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+
+            case "d":
+                var audio = new Audio("sounds/tom1.mp3");
+                audio.play();
+                break;
+
+            case "j":
+                var audio = new Audio("sounds/tom2.mp3");
+                audio.play();
+                break;
+
+            case "k":
+                var audio = new Audio("sounds/tom3.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("sounds/tom4.mp3");
+                audio.play();
+                break;
+
+            default:
+                console.log(buttonInnerHtml);
+
+        }
+    })
+}
